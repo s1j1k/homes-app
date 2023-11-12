8547e4106dc41936f057e5a9705867f20a7a8e29
Sally Kalumba <sallykalumba@gmail.com>
2023-11-12 18:54:33 +1100

    WIP adding application form

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,19 +1,18 @@
-import {Injectable} from '@angular/core';
-import {HousingLocation} from './housinglocation';
+import { Injectable } from "@angular/core";
+import { HousingLocation } from "./housinglocation";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HousingService {
-  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
-
+  readonly baseUrl = "https://angular.dev/assets/tutorials/common";
 
   protected housingLocationList: HousingLocation[] = [
     {
       id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
+      name: "Acme Fresh Start Housing",
+      city: "Chicago",
+      state: "IL",
       photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
       availableUnits: 4,
       wifi: true,
@@ -21,9 +20,9 @@ export class HousingService {
     },
     {
       id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
+      name: "A113 Transitional Housing",
+      city: "Santa Monica",
+      state: "CA",
       photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
       availableUnits: 0,
       wifi: false,
@@ -31,9 +30,9 @@ export class HousingService {
     },
     {
       id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
+      name: "Warm Beds Housing Support",
+      city: "Juneau",
+      state: "AK",
       photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
       availableUnits: 1,
       wifi: false,
@@ -41,9 +40,9 @@ export class HousingService {
     },
     {
       id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
+      name: "Homesteady Housing",
+      city: "Chicago",
+      state: "IL",
       photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
@@ -51,9 +50,9 @@ export class HousingService {
     },
     {
       id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
+      name: "Happy Homes Group",
+      city: "Gary",
+      state: "IN",
       photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
@@ -61,9 +60,9 @@ export class HousingService {
     },
     {
       id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
+      name: "Hopeful Apartment Group",
+      city: "Oakland",
+      state: "CA",
       photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
@@ -71,9 +70,9 @@ export class HousingService {
     },
     {
       id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
+      name: "Seriously Safe Towns",
+      city: "Oakland",
+      state: "CA",
       photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
       availableUnits: 5,
       wifi: true,
@@ -81,9 +80,9 @@ export class HousingService {
     },
     {
       id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
+      name: "Hopeful Housing Solutions",
+      city: "Oakland",
+      state: "CA",
       photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
@@ -91,9 +90,9 @@ export class HousingService {
     },
     {
       id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
+      name: "Seriously Safe Towns",
+      city: "Oakland",
+      state: "CA",
       photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
       availableUnits: 10,
       wifi: false,
@@ -101,9 +100,9 @@ export class HousingService {
     },
     {
       id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
+      name: "Capital Safe Towns",
+      city: "Portland",
+      state: "OR",
       photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
       availableUnits: 6,
       wifi: true,
@@ -115,6 +114,14 @@ export class HousingService {
     return this.housingLocationList;
   }
   getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+    return this.housingLocationList.find(
+      (housingLocation) => housingLocation.id === id
+    );
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
   }
 }
