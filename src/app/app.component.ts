a6c31555220bd5323fbc03f7ab1f85e0964653bb
Sally Kalumba <sallykalumba@gmail.com>
2023-11-12 19:02:57 +1100

    fix added bracket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterLink, RouterOutlet } from "@angular/router";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HomeComponent, RouterLink, RouterOutlet]],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
     <main>
       <a [routerLink]="['/']">
